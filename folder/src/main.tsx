161922c67e483f0d486a8460b490a7992a06ab54
Mac <mac@Meraj.local>
2025-02-10 08:29:40 +0530

    myPortfolio

---
+++ folder/src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.js/index.ts';
+import React from 'react'
+
+function main() {
+  return (
+    <div>createRoot(document.getElementById('root')!).render(
+       
+      );</div>
+  )
+}
+
+export default main
